354945be17be5db42ea8e6bfc1d42c28b1687686
Mário Menezes <mario082@gmail.com>
2021-03-30 19:23:25 -0300

    consumindo api com axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { genericControler } from './api/generic-api';
+import { People } from './api/schemas/people';
 
 const App = () => {
-
-    const [schema, setSchema] = useState({description:''});
-    const {getSchema} = genericControler('films');
-    getSchema().then(
-        valor => setSchema(valor)
-    )
+    const [schema, setSchema] = useState<People>();
+    const {getById} = genericControler<People>('people');
+    useEffect(() => {
+        getById(5).then(
+            valor => setSchema(valor)
+        )
+    })
+    
     return (
-        <div> {schema.description} </div>
-    )
+        <div> {schema? `O personagem ${schema.name} é do sexo ${schema.gender}`:null} </div> 
+    )  
 }
 
 export default App;
--- src/api/generic-api.ts
+++ src/api/generic-api.ts
@@ -3,13 +3,23 @@ import { getAxiosInstance } from "./axios-instance";
 
 export type SWAPIEndpoint = 'people' | 'films' | 'starships' | 'vehicles' | 'species' | 'planets';
 
-export const genericControler = (endpoint: SWAPIEndpoint) => {
-    const axios = getAxiosInstance();
+export interface ResourceReturn<T> {
+    getSchema:() => void;
+    getById:(id:number)=>Promise<T>;
+}
 
+export const genericControler =  <T>(endpoint: SWAPIEndpoint):ResourceReturn<T> => {
+    const axios = getAxiosInstance();
 
     const getSchema = async () => {
         const response = await axios.get(`/${endpoint}/schema`);
         return response.data;
     }    
-    return {getSchema}
+
+    const getById = async (id:number):Promise<T> => {
+        const response = await axios.get(`/${endpoint}/${id}`)
+        return response.data;
+    }
+
+    return {getSchema, getById}
 }
